6c8688e59b1074447820085a032612e8c9a1ea05
Albin Binu <albinbinuk17@gmail.com>
2025-05-17 19:51:15 +0530

    created app-color, and setted themes for app using class components

---
+++ lib/core/configs/theme/app_colors.dart
@@ -0,0 +1,13 @@
+
+
+import 'package:flutter/material.dart';
+
+class AppColors {
+
+  // colors for app(background,text,)
+  static const primary = Color(0xff42c83c);
+  static const lightBackground = Colors.white;
+  static const darkBackground = Colors.black;
+  static const grey = Color(0xffBEBEBE);
+  static const darkGrey = Color(0xff343434);
+}
---
+++ lib/core/configs/theme/app_theme.dart
@@ -0,0 +1,40 @@
+// This file is created for setting theme for the app, 
+// it includes app's light/dark backgroud theme data
+
+import 'package:flutter/material.dart';
+import 'package:spotifyclone/core/configs/theme/app_colors.dart';
+
+class AppTheme {
+  // themes are defined in app_color.dart
+  // setting app theme(light theme)
+  static final lightTheme = ThemeData(
+    primaryColor: AppColors.primary,
+    scaffoldBackgroundColor: AppColors.lightBackground,
+    brightness: Brightness.light,
+    elevatedButtonTheme: ElevatedButtonThemeData(
+      style: ElevatedButton.styleFrom(
+        backgroundColor: AppColors.primary,
+        textStyle: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+        shape: RoundedRectangleBorder(
+           borderRadius: BorderRadius.circular(30)
+        )
+      ),
+    ),
+  );
+  
+  // setting app theme(dark theme)
+   static final darkTheme = ThemeData(
+    primaryColor: AppColors.primary,
+    scaffoldBackgroundColor: AppColors.darkBackground,
+    brightness: Brightness.dark,
+    elevatedButtonTheme: ElevatedButtonThemeData(
+      style: ElevatedButton.styleFrom(
+        backgroundColor: AppColors.primary,
+        textStyle: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+        shape: RoundedRectangleBorder(
+           borderRadius: BorderRadius.circular(30)
+        )
+      ),
+    ),
+  );
+}
